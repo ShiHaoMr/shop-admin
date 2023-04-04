927e0eb878d3513aff48974707c8e6c62d1fcd0a
ShiHaoMr <205772626@qq.com>
2023-04-04 19:58:01 +0800

    newfolder(新建文件)

---
+++ shop/src/pages/404.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <el-col :sm="12" :lg="6">
+            <el-result icon="warning" title="Warning Tip" sub-title="Please follow the instructions">
+                <template #extra>
+                    <el-button type="primary" @click="$router.push('/')">返回首页</el-button>
+                </template>
+            </el-result>
+        </el-col>
+    </div>
+</template>
